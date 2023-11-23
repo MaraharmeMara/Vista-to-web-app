37b6038226e27f9783fa529a13ec8d3f7d7b5884
Ivan Cheremisin <fess932@gmail.com>
2023-11-23 04:26:43 +0300

    add ejs templates, add example index page with pannelum, todo: add example tour

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const app = express();
 const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express();
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get("/", index);
+app.get("/index", index);
+async function index(req, res) {
+  res.render("index", {
+    subject: "EJS template template engine",
+    name: "our template",
+    link: "https://google.com",
+  });
+}
