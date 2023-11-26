ca76597970692ff19298fcfdbaae5475abb9ba9e
Ivan Cheremisin <fess932@gmail.com>
2023-11-26 13:55:17 +0300

    create tour, hotstpo migration

---
+++ migrations/1700994445312_init.cjs
@@ -0,0 +1,59 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable(
+    "tour",
+    {
+      created_at: {
+        type: "timestamp",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
+      id: "id",
+      name: { type: "varchar(100)", notNull: true },
+      panorama_file: {
+        type: "varchar(300)",
+      },
+    },
+    {
+      ifNotExists: true,
+    }
+  );
+
+  pgm.createTable(
+    "hotspot",
+    {
+      created_at: {
+        type: "timestamp",
+        notNull: true,
+        default: pgm.func("current_timestamp"),
+      },
+      id: "id",
+      name: { type: "varchar(100)", notNull: true },
+      parent_id: {
+        type: "integer",
+        notNull: true,
+        references: "tour",
+      },
+      hotspot_type: {
+        type: "varchar(30)",
+        notNull: true,
+        check: "(hotspot_type in ('info', 'tour'))",
+      },
+      tour_id: {
+        type: "integer",
+        references: "tour",
+      },
+    },
+    {
+      ifNotExists: true,
+    }
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("tour", { ifExists: true, cascade: true });
+  pgm.dropTable("hotspot", { ifExists: true, cascade: true });
+};
